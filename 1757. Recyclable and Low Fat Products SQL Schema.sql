4a2e17e3586a7757125b8e9acef745886040b099
Surya <valmikisuriya@gmail.com>
2025-02-03 23:28:12 +0530

    Update 1757. Recyclable and Low Fat Products SQL Schema.sql

--- 1757. Recyclable and Low Fat Products SQL Schema.sql
+++ 1757. Recyclable and Low Fat Products SQL Schema.sql
@@ -45,7 +45,8 @@ Output:
 +-------------+
 Explanation: Only products 1 and 3 are both low fat and recyclable.
 */
-
+-- SQL Query
+	
 SELECT
 		product_id
 	FROM
@@ -57,6 +58,7 @@ SELECT
 
 /*
 Explanation:
+
 1. SELECT product_id: Retrieves the product_id column from the table.
 
 2. FROM Products: Specifies the Products table to query data from.
@@ -64,3 +66,13 @@ Explanation:
 3. WHERE low_fats = 'Y' AND recyclable = 'Y': Filters the records to include only those where both low_fats and recyclable are 'Y'.
 This will return the IDs of products that meet both conditions.
 */
+
+-- SQL Schema
+	
+Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'))
+Truncate table Products
+insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N')
+insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N')
