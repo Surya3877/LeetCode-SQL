3fee78d631add3607e52327c95e32c58b7979d19
Surya <valmikisuriya@gmail.com>
2025-02-22 20:29:21 +0530

    Update 1757. Recyclable and Low Fat Products.sql

--- 1757. Recyclable and Low Fat Products.sql
+++ 1757. Recyclable and Low Fat Products.sql
@@ -46,6 +46,20 @@ Output:
 Explanation: Only products 1 and 3 are both low fat and recyclable.
 */
 
+CREATE DATABASE IF NOT EXISTS 1757_Recyclable_and_Low_Fat_Products;
+USE 1757_Recyclable_and_Low_Fat_Products;
+
+CREATE TABLE Products (
+     product_id INT,            -- product_id is the primary key (column with unique values) for this table.
+     low_fats ENUM ('Y', 'N'),  -- low_fats is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+     recyclable ENUM ('Y', 'N') -- recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.  
+ );
+insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N');
+insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N');
+
 # SQL Query
 
 SELECT
@@ -57,24 +71,3 @@ WHERE
 AND
     recyclable = 'Y';
 
-/*
-Approach: Selecting rows based on conditions
-Algorithm
-The keyword SELECT is used to specify the columns that we want to retrieve 
-from the table Products. In this scenario, we want to retrieve the product_id column.
-
-The keyword WHERE is used to filter the rows in the table Products based on specific conditions, 
-which the low_fats column has the value "Y" (indicating low-fat products) and the recyclable 
-column has the value "Y" (indicating recyclable products). We use the logical operator AND to 
-combine both conditions, ensuring that the final result includes only product IDs for products
-that are both low fat and recyclable.
-*/
-
-
-Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'))
-Truncate table Products
-insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N')
-insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y')
-insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y')
-insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y')
-insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N')
